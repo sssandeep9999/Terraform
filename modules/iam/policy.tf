06b70c9cd12579cc9c780cb07bd7a1b0ee230cf2
sssandeep9999 <satyasaisandeep901@gmail.com>
2026-08-11 16:46:05 +0000

    eks describe policy added

--- modules/iam/policy.tf
+++ modules/iam/policy.tf
@@ -2,3 +2,24 @@ resource "aws_iam_role_policy_attachment" "ssm" {
   role       = aws_iam_role.ec2_ssm_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
+
+resource "aws_iam_role_policy" "eks_describe" {
+  name = "EKSDescribeCluster"
+  role = aws_iam_role.ec2_ssm_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Action = [
+          "eks:DescribeCluster"
+        ]
+
+        Resource = "arn:aws:eks:ap-south-1:294327208581:cluster/my-eks-cluster"
+      }
+    ]
+  })
+}
